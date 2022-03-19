4740e8ef036b8c5275ee4461f04fe70c30c709f4
최병채 <68932465+chlqudco@users.noreply.github.com>
2022-03-19 23:57:05 +0900

    [Silver I] Title: 신입 사원, Time: 392 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,26 +1,26 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-typedef pair<int, int> p;
-
-p arr[100001];
-int T, N, min_second, cnt;
+int arr[100001];
+int T, N, cnt,min_second;
 
 void Insert_And_Init() {
 	cin >> T;
 }
 
 void Calculate() {
+	int a, b;
 	for (int i = 0; i < T; i++){
 		cin >> N;
-		for (int j = 0; j < N; j++) cin >> arr[j].first >> arr[j].second;
-		sort(arr, arr + N);
+		for (int j = 0; j < N; j++) {
+			cin >> a >> b;
+			arr[a] = b;
+		}
 		cnt = 1;
-		min_second = arr[0].second;
-		for (int j = 1; j < N; j++) {
-			if (min_second > arr[j].second) {
-				min_second = arr[j].second;
+		min_second = arr[1];
+		for (int j = 2; j <= N; j++) {
+			if (min_second > arr[j]) {
+				min_second = arr[j];
 				cnt++;
 			}
 		}
