d80ff6f117ee5865e7e7ff78e78efff6c7fbdf98
최병채 <68932465+chlqudco@users.noreply.github.com>
2022-07-04 16:35:08 +0900

    [Gold IV] Title: 좋다, Time: 16 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -1,51 +1,47 @@
 #include <iostream>
-#include <map>
-#include <vector>
 #include <algorithm>
+#include <math.h>
 using namespace std;
 
 int N;
-vector<int> v;
-map<int,int> ms;
+int num[2001] = { 0 };
 
 void Insert_And_Init() {
-    int tmp;
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> tmp;
-        //해당 값의 인덱스와 몇번 나오는지
-        ms[tmp]++;
-        v.push_back(tmp);
-    }
-    sort(v.begin(), v.end());
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> num[i];
+	sort(num, num + N);
 }
 
 void Caluclate() {
-    int total = 0;
-    if (N <= 2) {
-        cout << total;
-        return;
-    }
-   //모든 원소를 돌믄서
-    for (int i = 0; i < N-1; i++){
-        for (int j = i + 1; j < N; j++) {
-            //둘중 0이 있으면 예외처리
-            if (v[j] == 0 || v[i] == 0) {
-                if (v[j] == 0 && v[i] == 0 && ms[0] == 2) continue;
-                else if (ms[v[i] + v[j]] == 1) continue;
-            }
-            total += ms[v[i] + v[j]];
-            ms[v[i] + v[j]] = 0;
-        }
-    }
+	int cnt = 0;
 
-    cout << total;
+	for (int i = 0; i < N; i++){
+		int target = num[i];
+		int start = 0, end = N - 1;
+
+		bool flag = false;
+		while (start < end) {
+			if ((num[start] + num[end] == target) && start != i && end != i) {
+				cnt++;
+				break;
+			}
+
+			if (num[start] + num[end] == target) {
+				if (flag) start++;
+				else end--;
+				flag = !flag;
+			}
+			else if(num[start] + num[end] > target) end--;
+			else start++;
+		}
+	}
+	cout << cnt;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    Insert_And_Init();
+	Insert_And_Init();
 
-    Caluclate();
+	Caluclate();
 }
