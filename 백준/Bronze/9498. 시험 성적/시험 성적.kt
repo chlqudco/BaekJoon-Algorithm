8c23357990ca2bc981421e026fed14d66ba09d33
최병채 <68932465+chlqudco@users.noreply.github.com>
2023-04-15 13:01:21 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 12264 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,24 @@
+fun main() = with(System.`in`.bufferedReader()) {
+
+    when (readLine().toInt()) {
+        in 90..100 -> {
+            print("A")
+        }
+
+        in 80..89 -> {
+            print("B")
+        }
+
+        in 70..79 -> {
+            print("C")
+        }
+
+        in 60..69 -> {
+            print("D")
+        }
+
+        else -> {
+            print("F")
+        }
+    }
+}
