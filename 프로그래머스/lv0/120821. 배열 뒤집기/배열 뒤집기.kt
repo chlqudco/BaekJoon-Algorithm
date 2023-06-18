af8b7d59fb0a48bd9a43e18c9fd36175c86376e6
최병채 <68932465+chlqudco@users.noreply.github.com>
2023-06-18 18:35:05 +0900

    [level 0] Title: 배열 뒤집기, Time: 13.61 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        answer = num_list.reversedArray()
+        
+        return answer
+    }
+}
