0da32eac133fb28176b17d5267302499d0598102
최병채 <68932465+chlqudco@users.noreply.github.com>
2023-06-18 19:21:17 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 10.45 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        answer += num_list.count{ it % 2 == 0}
+        answer += num_list.count{ it % 2 == 1}
+        
+        return answer
+    }
+}
