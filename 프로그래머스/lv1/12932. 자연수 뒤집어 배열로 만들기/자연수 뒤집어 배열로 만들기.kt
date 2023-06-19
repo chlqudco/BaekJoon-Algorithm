7c67e54f92dfd78a1a5fba727476e34beeced76e
최병채 <68932465+chlqudco@users.noreply.github.com>
2023-06-19 20:30:57 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 13.95 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(n: Long): IntArray {
+        var answer = intArrayOf()
+        
+        var num = n
+        while(num > 0){
+            answer = answer + (num % 10).toInt()
+            num /= 10
+        }
+        
+        return answer
+    }
+}
