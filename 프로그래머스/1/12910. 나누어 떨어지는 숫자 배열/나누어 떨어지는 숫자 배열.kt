4038d117931fb8ea9b43ae96da7338e17112e1bf
최병채 <68932465+chlqudco@users.noreply.github.com>
2025-10-30 23:09:50 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 88.99 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(arr: IntArray, divisor: Int): IntArray {
+        var answer = intArrayOf()
+        
+        var arr2 = arr.copyOf().sorted()
+        
+        arr2.forEach{
+            if(it % divisor == 0) answer += it
+        }
+        
+        if(answer.isEmpty()) answer += -1
+        
+        return answer
+    }
+}
